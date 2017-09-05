452a0d3a9dfadb52096d7e1403ca03de75fb6a91
mrtommyb <tom@tombarclay.com>
2017-09-05 18:52:37 +0530

    adding some basic test cases

--- ticgen/tests/test_basics.py
+++ ticgen/tests/test_basics.py
@@ -1,7 +1,7 @@
 """Basic sanity checks to verify that tvguide works.
 To run, simply type "py.test".
 """
-
+import numpy as np
 
 def test_import():
     """Can we import tvguide successfully?"""
@@ -10,11 +10,35 @@ def test_import():
     from ticgen import ticgen
     from ticgen import Star
 
+
 def test_VJKs():
     from ticgen import Star
     star = Star(Vmag=12, Ksmag=10, Jmag=10.5)
-    assert star.Vmag
-    assert star.Ksmag
-    assert star.Jmag
-    assert star.Tmag
+    assert np.allclose(star.Vmag, 12.0)
+    assert np.allclose(star.Ksmag, 10.0)
+    assert np.allclose(star.Jmag, 10.5)
+#    assert np.allclose(star.Tmag,) # todo - calc from the paper
+#    assert np.allclose(star.oneSigmaNoise,) # todo - calc from the paper
     assert star.TmagProvenance == 'V/J/Ks'
+
+
+def test_BphJKs():
+    from ticgen import Star
+    star = Star(Bphmag=12, Ksmag=10, Jmag=10.5)
+    assert np.allclose(star.Bphmag, 12.0)
+    assert np.allclose(star.Ksmag, 10.0)
+    assert np.allclose(star.Jmag, 10.5)
+#    assert np.allclose(star.Tmag,) # todo - calc from the paper
+#    assert np.allclose(star.oneSigmaNoise,) # todo - calc from the paper
+    assert star.TmagProvenance == 'Bph/J/Ks'
+
+
+def test_BJKs():
+    from ticgen import Star
+    star = Star(Bmag=12, Ksmag=10, Jmag=10.5)
+    assert np.allclose(star.Bmag, 12.0)
+    assert np.allclose(star.Ksmag, 10.0)
+    assert np.allclose(star.Jmag, 10.5)
+#    assert np.allclose(star.Tmag,) # todo - calc from the paper
+#    assert np.allclose(star.oneSigmaNoise,) # todo - calc from the paper
+    assert star.TmagProvenance == 'B/J/Ks'
